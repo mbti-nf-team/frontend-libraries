91da6dedeb36b171b09330204744cb1d6f6bc328
saseungmin <dbd02169@naver.com>
2023-07-02 22:54:53 +0900

    feat: DelayRenderComponent, useBoolean 추가

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,3 @@
 export { default as GlobalPortal } from './components/GlobalPortal';
+export { default as DelayRenderComponent } from './components/DelayRenderComponent';
 export { default as useIsMounted } from './hooks/useIsMounted';
