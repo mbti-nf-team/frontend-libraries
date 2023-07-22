dbe425cef9d84aadcdb0f900795c5d42ec8b3978
saseungmin <dbd02169@naver.com>
2023-07-22 13:42:41 +0900

    chore: frontend library 문서 세팅

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   ignorePatterns: [
     'node_modules/',
     'public/',
+    'build/',
+    'coverage/',
   ],
   extends: ['@nft-team/eslint-config'],
   overrides: [
@@ -18,5 +20,17 @@ module.exports = {
         project: ['./tsconfig.json'],
       },
     },
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
   ],
+  rules: {
+    'import/export': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
 };
