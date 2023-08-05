6be6edc4e7185c90340c6ad11c8524bd3ffb5871
saseungmin <dbd02169@naver.com>
2023-08-05 18:53:52 +0900

    chore: docs sass, svg element 사용으로 마이그레이션

--- apps/docs/src/components/Feature/index.tsx
+++ apps/docs/src/components/Feature/index.tsx
@@ -1,19 +1,21 @@
+import { ComponentType, SVGProps } from 'react';
+
 import clsx from 'clsx';
 
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
 type Props = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Thumbnail: ComponentType<SVGProps<SVGSVGElement>>;
 };
 
 function Feature({
-  title, Svg,
+  title, Thumbnail,
 }: Props) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Thumbnail className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
