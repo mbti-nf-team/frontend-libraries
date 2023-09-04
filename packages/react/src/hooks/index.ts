28a2a26027621c167eae7ea03c38819ad760f059
saseungmin <dbd02169@naver.com>
2023-09-05 00:43:23 +0900

    feat(hook): useUpdateEffect, useIsFirstRender hook 구현

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,9 +1,11 @@
 export { default as useActionKeyEvent } from './useActionKeyEvent';
 export { default as useBoolean } from './useBoolean';
 export { default as useDebounce } from './useDebounce';
+export { default as useIsFirstRender } from './useIsFirstRender';
 export { default as useIsMounted } from './useIsMounted';
 export { default as useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { default as useLessThenScrollY } from './useLessThenScrollY';
 export { default as useResizeViewportHeight } from './useResizeViewportHeight';
 export { default as useThrottleCallback } from './useThrottleCallback';
 export { default as useTimeout } from './useTimeout';
+export { default as useUpdateEffect } from './useUpdateEffect';
