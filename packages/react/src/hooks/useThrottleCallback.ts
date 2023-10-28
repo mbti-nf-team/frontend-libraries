05d9535cdb92faa55cc6a5eff550abc9c24a67e4
saseungmin <dbd02169@naver.com>
2023-10-28 22:18:06 +0900

    fix(@nf-team/react): useThrottleCallback dependencyList 추가

--- packages/react/src/hooks/useThrottleCallback.ts
+++ packages/react/src/hooks/useThrottleCallback.ts
@@ -3,6 +3,7 @@ import { useCallback, useRef } from 'react';
 function useThrottleCallback<U extends never[]>(
   callback: (...args: U) => void,
   delay: number,
+  dependencyList: any[],
 ) {
   const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
 
@@ -13,7 +14,7 @@ function useThrottleCallback<U extends never[]>(
         timer.current = null;
       }, delay);
     }
-  }, [callback, delay]);
+  }, [...dependencyList, delay]);
 
   return throttledCallback;
 }
